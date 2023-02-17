37285725ca71a36e61b5a5adb409bc882d7664c9
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-02-17 21:09:01 +0000

    set up djangoadd and setup apps from home,blog and ngo

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    """
+    modle for blog page
+    """
+
+    title = models.CharField(max_length=250, null=False, default='Add Blog title here', unique=True)
+    body = models.TextField(null=False, default='Add Blog content here')
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+            ordering = ['-added_on']
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Blog
+
+
+
+def blog(request):
+    """
+    view for blod page
+    """
+    blog = Blog.objects.all()
+
+    template = 'blog/blog.html'
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, template, context)
