807bc911e344317d06c0f6891afe67ae1ef3e7ed
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-02-18 09:03:35 +0000

    add .env file and secrert key config in settings.py

--- hopelink/settings.py
+++ hopelink/settings.py
@@ -21,7 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ar=yg!f4e&wptya7&dgws!cin!(xqxu#a_fqo293#$9c)xormk'
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
