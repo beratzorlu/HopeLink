f9d6e5814933b6cd85982c29bbb9af5ba81010e6
SamuelUkachukwu <shybold@yahoo.com>
2023-02-20 14:24:03 +0000

    Added detail view for blog and ngo's.

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('more_info/<int:ngo_id>/', views.ngo_detail_views, name='detail_view'),
+    path('read_blog/<int:blog_id>/', views.blog_detail_views, name='blog_view'),
     path('search/', views.search, name='search'),
 ]
